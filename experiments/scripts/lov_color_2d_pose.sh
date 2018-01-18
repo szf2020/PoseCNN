75634f8e4351f5e9bebbba1ae4357527a7c52c99
yuxng <xyfudan@gmail.com>
2018-01-17 22:33:40 -0500

    test triangle

--- experiments/scripts/lov_color_2d_pose.sh
+++ experiments/scripts/lov_color_2d_pose.sh
@@ -30,7 +30,7 @@ echo Logging output to "$LOG"
 # test FCN for single frames
 time ./tools/test_net.py --gpu 0 \
   --network vgg16_convs \
-  --model output/lov/lov_train/vgg16_fcn_color_single_frame_2d_pose_triangle_lov_iter_40000.ckpt \
+  --model output/lov/lov_train/vgg16_fcn_color_single_frame_2d_pose_triangle_lov_iter_60000.ckpt \
   --imdb lov_keyframe \
   --cfg experiments/cfgs/lov_color_2d_pose.yml \
   --rig data/LOV/camera.json \
